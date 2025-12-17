b60a5646f11a394b98f5b5163c70eb1fdcd42a97
gagangagan0104 <gagangagan0104@gmail.com>
2025-12-17 14:19:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -97,6 +97,7 @@ async def main():
     logging.info("Bot started")
     
     # Запускаем polling бота
+        await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
